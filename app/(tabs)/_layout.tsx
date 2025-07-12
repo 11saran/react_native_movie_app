a4bdc363e0bf1cde6d2ef70f8c4dc1fe671eeb77
Tharsan01 <1211saran@gmail.com>
2025-07-12 22:44:28 +0530

    Add tab navigation and screens for Home, Search, Saved, and Profile

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{ title: "saved", headerShown: false }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ title: "Profile", headerShown: false }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
