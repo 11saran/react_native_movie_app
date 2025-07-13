fa6e054fcdf72b89253fe17a3965b3afe233b179
Tharsan01 <1211saran@gmail.com>
2025-07-13 23:21:34 +0530

    Update home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full"/>
     </View>
   );
 }
